5355e7bec0bba31de19fa1a5e2551a75fc4e606e
Aniruddh Gopale <anigopale@gmail.com>
2017-08-28 14:29:51 +0530

    video list created by grabbing video attributes

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -8,10 +8,18 @@ import React from 'react';
 
 //es6 alternative
 const VideoListItem = ({video}) => {
-  console.log(video);
+  const imageUrl = video.snippet.thumbnails.default.url;
   return (
     <li className="list-group-item ">
-    Video</li>
+        <div className="video-list media">
+          <div className="media-left">
+            <img className="media-object" src={imageUrl}/>
+          </div>
+          <div className="media-body">
+            <div className="media-heading">{video.snippet.title}</div>
+          </div>
+        </div>
+    </li>
   );
 };
 
