29e78c9ce7706a584e909075b371a4ca28a9c568
Aniruddh Gopale <anigopale@gmail.com>
2017-08-28 21:25:55 +0530

    video selection created

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,15 +2,20 @@ import React from 'react';
 
 // const VideoListItem = (props) => {
 //   const video = props.video;
-//
+//   const onVideoSelect = props.onVideoSelect;
 //   return <li>Video</li>;
 // };
 
+// const VideoListItem = ({video, onVideoSelect}) => {
+//   return <li>Video</li>;
+// }
+
 //es6 alternative
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   const imageUrl = video.snippet.thumbnails.default.url;
+  
   return (
-    <li className="list-group-item ">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item ">
         <div className="video-list media">
           <div className="media-left">
             <img className="media-object" src={imageUrl}/>
